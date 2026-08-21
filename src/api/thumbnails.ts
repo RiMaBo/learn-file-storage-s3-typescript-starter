134f1164651d50241f37783789697ac087e5c0fc
RiMaBo <rimabo@gmail.com>
2026-08-21 01:12:17 -0600

    Cache Headers

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,3 +1,5 @@
+import path from "path";
+
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
 import { getVideo, updateVideo } from "../db/videos";
@@ -26,14 +28,14 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
     throw new BadRequestError("Error: Missing Content-Type for thumbnail");
   }
 
+  const splitMediaType = mediaType.split("/")
+  if ((splitMediaType[0] !== "image") || (splitMediaType[1].length < 1)) {
+    throw new BadRequestError("Error: Invalid Content-Type for thumbnail");
+  }
+
   const MAX_UPLOAD_SIZE = 10 << 20  // 10MB
   if (file.length > MAX_UPLOAD_SIZE) {
     throw new BadRequestError("Error: Thumbnail file should be 10MB or less");
-  }
-
-  const fileData = Buffer.from(await file.arrayBuffer()).toString("base64");
-  if (!fileData) {
-    throw new BadRequestError("Error reading file data");
   }
 
   const video = await getVideo(cfg.db, videoId);
@@ -44,7 +46,11 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
     throw new UserForbiddenError("Error: You are not allowed to upload a thumbnail for this video");
   }
 
-  const thumbnailURL = `data:${mediaType};base64,${fileData}`;
+  const fileName = `${videoId}.${splitMediaType[1]}`;
+  const filePath = path.join(cfg.assetsRoot, fileName);
+  await Bun.write(filePath, file);
+
+  const thumbnailURL = `http://localhost:${cfg.port}/assets/${fileName}`;
   video.thumbnailURL = thumbnailURL;
   updateVideo(cfg.db, video);
 
